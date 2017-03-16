14005922d2bcae712b4924c72418d6dbf1e9a521
Will <willbeas88@gmail.com>
2017-03-16 01:19:07 -0500

    Fixed issue with KNN Added example to show testing knn and linear models with same data

--- examples/integration_examples.py
+++ examples/integration_examples.py
@@ -3,20 +3,20 @@ import pandas as pd
 from sklearn_selection import PMDiscoveryTool
 
 
-def pima_example():
+def knn_and_linear():
     url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data'
     col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
     pima = pd.read_csv(url, header=None, names=col_names)
 
     feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
 
-    pima_data = PMDiscoveryTool(pima, pima[feature_cols], pima.label)
-    pima_data.set_feature_cols(feature_cols)
+    pima_data = PMDiscoveryTool(pima, pima, pima.label)
+    print("KNeighbors")
+    pima_data.kneighbors_classifier_selection()
 
+    # Set feature columns on x to perform compare against linear models
+    pima_data.set_feature_cols(feature_cols)
     pima_data.logistic_regression_selection()
     pima_data.linear_regression_selection()
 
-
-pima_example()
-
-
+knn_and_linear()
--- sklearn_selection.py
+++ sklearn_selection.py
@@ -1,8 +1,11 @@
+import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, RandomizedSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 
 class PMDiscoveryTool:
@@ -18,6 +21,7 @@ class PMDiscoveryTool:
         self.x = self.data[feature_cols]
 
     def logistic_regression_selection(self):
+        print("\nLogistic Regression Accuracy")
         log_reg = LogisticRegression()
         x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, random_state=0)
         log_reg.fit(x_train, y_train)
@@ -28,20 +32,22 @@ class PMDiscoveryTool:
             feature_selection_cnt = sum([sub_list[feature] for sub_list in log_reg_cm])
             predict_percent = int((feature_selection_cnt/total_predictions) * 100)
             accuracy = int((log_reg_cm[feature][feature]/sum(log_reg_cm[feature])) * 100)
-            print('Logistic Regression Accuracy: '
-                  'Col {} - (Accuracy: {}%, Predicted: {}%)'.format(feature, accuracy, predict_percent))
+            print('Col {} - (Accuracy: {}%, Predicted: {}%)'.format(feature, accuracy, predict_percent))
 
     def linear_regression_selection(self):
         lm = LinearRegression()
         scores = cross_val_score(lm, self.x, self.y, cv=10, scoring='neg_mean_squared_error')
         scores = -scores
         rmse_scores = np.sqrt(scores)
-        print('Linear Regression Accuracy: {}%'.format(int((1-rmse_scores.mean())*100)))
+        print('\nLinear Regression Accuracy: {}%'.format(int((1 - rmse_scores.mean()) * 100)))
 
     def kneighbors_classifier_selection(self, min_range=1, max_range=31, weight_options=[]):
         k_range = range(min_range, max_range)
         knn = KNeighborsClassifier(n_neighbors=5)
-        grid_dict = dict(n_neighbors=k_range, weights=weight_options)
+        grid_dict = dict(n_neighbors=k_range)
+
+        if weight_options:
+            grid_dict['weights'] = weight_options
 
         if self.expensive:
             grid = RandomizedSearchCV(knn, grid_dict, cv=10, scoring='accuracy', n_iter=10, random_state=5, n_jobs=-1)
@@ -53,6 +59,3 @@ class PMDiscoveryTool:
         print(grid.best_score_)
         print(grid.best_params_)
         print(grid.best_estimator_)
-
-
-
